9e867fc0b3684a8c21cfd219d60b80a2b4d44b86
Arash Sheyda <sheidaeearash1999@gmail.com>
2023-06-18 13:04:06 +0300

    docs: favicon

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,18 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
 
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/mongoose-icon.svg',
+        },
+      ],
+    },
+  },
+
   modules: [
     '@nuxthq/studio',
   ],
