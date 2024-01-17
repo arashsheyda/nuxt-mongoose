42b3b8375a64ce12082d831432a9efb1c2835910
Arash <arashi.sheyda@gmail.com>
2024-01-17 13:57:49 -0700

    chore: update playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,14 @@
-<script setup>
+<script setup lang="ts">
+const { data } = await useFetch('/api/users')
 </script>
 
 <template>
   <div>
     Nuxt module playground!
+    <br>
+    <pre
+      v-html="JSON.stringify(data, null, 2)"
+      style="background: #131313;color: #4EA65A;padding: 20px;border-radius: 7px;text-shadow: 0px 0px 10px #00ff22;"
+    />
   </div>
 </template>
